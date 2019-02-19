e26b1094c83e5098e70f6f8de9dcf66610c254b1
tphuong141607 <tphuong141607@gmail.com>
2019-02-19 16:03:10 -0500

    update func sushi_unquote(char *s) - modify its own string instead of creating a new one

--- src/sushi_parse.c
+++ src/sushi_parse.c
@@ -4,38 +4,45 @@
 #include "sushi_yyparser.tab.h"
 
 char *sushi_unquote(char *s) {
-    int len = strlen(s);
-    char *newString = malloc((len + 1)); // +1 b/c of NULL
-    
     int sINDX = 0;
-    int newStringINDX = 0;
+    int len = strlen(s);
     
     while (s[sINDX] != 0) {
-        if (s[sINDX] == '\\') {
+        // If whitespace characters, enter this part of the code.
+        if ((s[sINDX] == '\\') && (s[sINDX+1] != 0)) {
             switch(s[sINDX+1]){
-                case 'a': newString[newStringINDX] = '\a'; break;
-                case 'b': newString[newStringINDX] = '\b'; break;
-                case 'f': newString[newStringINDX] = '\f'; break;
-                case 'n': newString[newStringINDX] = '\n'; break;
-                case 'r': newString[newStringINDX] = '\r'; break;
-                case 't': newString[newStringINDX] = '\t'; break;
-                case 'v': newString[newStringINDX] = '\v'; break;
-                case '\\': newString[newStringINDX] = '\\'; break;
-                case '\'': newString[newStringINDX] = '\''; break;
-                case '"': newString[newStringINDX] = '\"'; break;
-                case '?': newString[newStringINDX] = '\?'; break;
+                case 'a': s[sINDX] = '\a'; break;
+                case 'b': s[sINDX] = '\b'; break;
+                case 'f': s[sINDX] = '\f'; break;
+                case 'n': s[sINDX] = '\n'; break;
+                case 'r': s[sINDX] = '\r'; break;
+                case 't': s[sINDX] = '\t'; break;
+                case 'v': s[sINDX] = '\v'; break;
+                case '\\': s[sINDX] = '\\'; break;
+                case '\'': s[sINDX] = '\''; break;
+                case '"': s[sINDX] = '\"'; break;
+                case '?': s[sINDX] = '\?'; break;
             }
-            printf("%c", newString[newStringINDX]);
-            newStringINDX++;
-            sINDX+=2;
-            
-        } else {
-                newString[newStringINDX] = s[sINDX];
-                newStringINDX++;
-                sINDX++;
+            /* If there is nothing after the whitespace characters,
+               simply assign NULL terminator to s[sINdx + 1]
+               Example: '\' and 'n' --> '\n' and '\0' */
+            if (s[sINDX + 2] == '\0') {
+                s[sINDX + 1] = '\0';
+                
+            } else {
+                /* If we still have some characters after the whitespace characters
+                   shift all characters starting from sINDX+1 until the end of the string
+                   Example: '\' and 'n' and 'a' --> '\n' and 'a' */
+                for (int i = sINDX+1; i < len; i++) {
+                    s[i] = s[i + 1];
+                }
+            }
         }
+        // If not whitespace characters, simply increment
+        sINDX++;
     }
-    return newString;
+    sINDX++;
+    return s;
 }
 
 // Do not modify this function
